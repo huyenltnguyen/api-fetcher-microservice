ea51b4d5e761c6e1a16230585d5d5788f0b2d971
huyenltnguyen <25715018+huyenltnguyen@users.noreply.github.com>
2018-08-22 17:16:34 +0700

    implement fetch api logic

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import axios from 'axios';
+import CircularJSON from 'circular-json';
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,8 +9,34 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello' });
+app.post('/api', (req, res) => {
+  const data = req.body;
+  let apiUrl = '';
+
+  if (data.appName === 'weather app') {
+    if (!data.latitude || !data.longitude) {
+      res.json({ message: 'Missing geolocation data.' });
+    } else {
+      apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${data.latitude}&lon=${data.longitude}&units=metric&type=accurate&APPID=${process.env.WEATHER_APP_API_KEY}`;
+    }
+  } else {
+    res.json({ message: 'App not found.' });
+  }
+
+  axios.get(apiUrl)
+    .then((d) => {
+      // Use circular-json package to resolve
+      // the 'Converting circular structure to JSON' error
+      const json = CircularJSON.stringify(d);
+      res.json({
+        message: 'Data successfully fetched',
+        data: json,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({ message: 'Something went wrong.' });
+    });
 });
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
